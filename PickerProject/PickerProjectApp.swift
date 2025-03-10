6b774119d7345c026ad30b44700a2ac970cb41b0
4OH4Loop <semperchuck@gmail.com>
2025-03-10 18:06:12 -0400

    Initial Commit

---
+++ PickerProject/PickerProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickerProjectApp.swift
+//  PickerProject
+//
+//  Created by Carolyn Ballinger on 3/10/25.
+//
+
+import SwiftUI
+
+@main
+struct PickerProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

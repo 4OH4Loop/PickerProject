2d34dc38103b0afe28111777a279bff47a3be648
4OH4Loop <semperchuck@gmail.com>
2025-03-10 18:27:40 -0400

    In class

--- PickerProject/ContentView.swift
+++ PickerProject/ContentView.swift
@@ -8,12 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedCountry: String? = nil
+    let countries = ["Australia", "USA", "UK", "China"]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("This is a menu picker:")
+                .font(.largeTitle)
+            
+            HStack {
+                Text("Select a country:")
+                
+                Spacer()
+                
+                Picker("NO TITLE!", selection: $selectedCountry) {
+                    
+                    Text("No country selected")
+                        .tag(nil as String?)
+                    
+                    ForEach(countries, id: \.self) { country in
+                        Text(country)
+                            .tag(country as String?)
+                    }
+                }
+            }
+            .font(.largeTitle)
+
+//            Text("Current selection is: \(selectedCountry)")
+//                .font(.largeTitle)
         }
         .padding()
     }
